70fedabf94d2577010545b31f6188f91bfdc5fab
Maaz <muhammed.maaz1230@gmail.com>
2025-09-30 00:29:29 +0500

    fix: vercel config for vite

--- vite.config.ts
+++ vite.config.ts
@@ -11,12 +11,15 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    outDir: "dist", // Vercel expects this
+  },
+  base: "/", // ensures React Router refresh works
 }));
